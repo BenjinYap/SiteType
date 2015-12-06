a3b22e88adaaf1798dec981f56321666f7c9fae8
Benjin Yap <benjyap.dev@gmail.com>
2015-12-06 17:41:29 -0500

    typing works

--- core.js
+++ core.js
@@ -11,6 +11,8 @@ var module = (function (module, $) {
 			});
 		});
 
+		//elements [0].element.text ('aaa');
+
 		for (var i = 0; i < elements.length; i++) {
 			var e = elements [i];
 
@@ -20,19 +22,55 @@ var module = (function (module, $) {
 
 			for (var j = 0; j < text.length; j++) {
 				var c = text.charAt (j);
-				e.chars = c;
+				e.chars.push (c);
 				html += '<span>' + c + '</span>';
 			}
 
 			e.element.html (html);
 		}
 
+		var $input = $('<input type = "text" id = "input-dump" />');
+		$('body').prepend ($input);
+		$input.focus ();
+
+		var prevValue = '';
+
 		activateElement (elements [0]);
 
-		function activateElement (e) {
-			var $e = e.element;
+		function activateElement (eObj) {
+			var $e = eObj.element;
+			$e.addClass ('active');
 
+			var charIndex = 0;
+			activateChar ($e.find ('span:eq(' + charIndex + ')'));
 
+			$input.on ('input', function (e) {
+				var value = $(this).val ();
+
+				for (var i = 0; i < prevValue.length; i++) {
+					value = value.replace (prevValue.charAt (i), '');
+				}
+
+				for (var i = 0; i < value.length; i++) {
+					console.log (value.charAt (i), eObj.chars [charIndex]);
+					if (eObj.chars [charIndex] === value.charAt (i)) {
+						charIndex++;
+						deactivateChar ($e.find ('span:eq(' + (charIndex - 1) + ')'));
+						activateChar ($e.find ('span:eq(' + charIndex + ')'));
+					}
+				}
+
+				prevValue = $(this).val ();
+			});
+		}
+
+		function deactivateChar ($c) {
+			$c.removeClass ('active-char');
+			$c.addClass ('deactive-char');
+		}
+
+		function activateChar ($c) {
+			$c.addClass ('active-char');
 		}
 	};
 
