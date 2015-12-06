a05f912e86264a50ac42bad81a0ac79a80c2b44c
Benjin Yap <benjyap.dev@gmail.com>
2015-12-06 16:27:32 -0500

    spans all paragraphs

--- core.js
+++ core.js
@@ -2,7 +2,37 @@ var module = (function (module, $) {
 	module.doIt = function () {
 		console.log ('working');
 
-		
+		var elements = [];
+
+		$('p').each (function () {
+			elements.push ({
+				element:$(this),
+				chars:[],
+			});
+		});
+
+		for (var i = 0; i < elements.length; i++) {
+			var e = elements [i];
+
+			var text = e.element.text ();
+			e.element.html ('');
+			var html = '';
+
+			for (var i = 0; i < text.length; i++) {
+				var c = text.charAt (i);
+				e.chars = c;
+				html += '<span>' + c + '</span>';
+			}
+
+			e.element.html (html);
+		}
+
+		activateElement (elements [0]);
+
+		function activateElement (e) {
+			var $e = e.element;
+			
+		}
 	};
 
 	return module;
